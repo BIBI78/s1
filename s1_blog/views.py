bc5f1e645a38c3bd895d8e941448bc6bd6c5a736
BIBI78 <blakecollins78@gmail.com>
2023-09-17 12:19:55 +0000

    updates to user profile

--- s1_blog/views.py
+++ s1_blog/views.py
@@ -14,11 +14,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-# class PostList(generic.ListView):
-#     model = Post
-#     queryset = Post.objects.filter(status=1).order_by("-created_on")
-#     template_name = "index.html"
-#     paginate_by = 6
 
 
 class PostDetail(View):
