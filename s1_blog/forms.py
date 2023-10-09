3b0e2731fd1e577e51397035adcb3fd760ea234b
BIBI78 <blakecollins78@gmail.com>
2023-10-09 12:48:41 +0000

    FORMAT: format html files, remove scrap papper

--- s1_blog/forms.py
+++ s1_blog/forms.py
@@ -6,12 +6,6 @@ from .models import UserProfile
 
 
 
-# class CommentForm(forms.ModelForm):
-#     body = forms.CharField(label='')
-
-#     class Meta:
-#         model = Comment
-#         fields = ('body',)
 class CommentForm(forms.ModelForm):
     body = forms.CharField(
         label='',  # Set an empty label
@@ -23,17 +17,6 @@ class CommentForm(forms.ModelForm):
         fields = ['body']
 
 
-
-# class CreatePostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         fields = ['title', 'content', 'kilometers_ran','location', 'duration', 'featured_image']  
-
-  
-#     kilometers_ran = forms.DecimalField(label='Kilometers Ran', required=True)
-#     location = forms.CharField(max_length=100, label='Location', required=True)
-#     duration = forms.DurationField(label='Duration', required=True)
-#     featured_image = forms.ImageField(label='Featured Image', required=False)
 
 
 
